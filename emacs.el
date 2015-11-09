3fb0bd23aded2df87b6d5df674bf0556905f3fa1
Johannes Gustafsson <johannes@inexchange.se>
2015-11-09 11:57:54 +0100

    emacs

--- emacs.el
+++ emacs.el
@@ -1,8 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("196cc00960232cfc7e74f4e95a94a5977cb16fd28ba7282195338f68c84058ec" default))))
+
 (require 'package)
 
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+;(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 (setq package-enable-at-startup nil)
 
@@ -27,7 +36,28 @@ Return a list of installed packages or nil for every skipped package."
 (package-initialize)
 
 (ensure-package-installed 'evil
-                          'helm)
+			  'evil-leader
+                          'helm
+			  'monokai-theme)
 (require 'evil)
+(require 'evil-leader)
+(require 'helm-config)
+
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+
+(evil-leader/set-key
+  "f" 'find-file
+  "b" 'switch-to-buffer)
 
 (evil-mode t)
+
+(load-theme 'monokai)
+
+;(set-face-attribute 'default nil :font "Consolas-11.0")
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
