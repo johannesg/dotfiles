4893bcf36dd7352b0f93360706b2dbc1092e108c
Johannes Gustafsson <johannes@inexchange.se>
2015-11-09 16:28:32 +0100

    emacs

--- emacs.el
+++ emacs.el
@@ -5,7 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("196cc00960232cfc7e74f4e95a94a5977cb16fd28ba7282195338f68c84058ec" default))))
+    ("196cc00960232cfc7e74f4e95a94a5977cb16fd28ba7282195338f68c84058ec" default)))
+ '(inhibit-startup-screen t))
 
 (require 'package)
 
@@ -54,10 +55,6 @@ Return a list of installed packages or nil for every skipped package."
 
 (load-theme 'monokai)
 
+(helm-mode t)
+
 ;(set-face-attribute 'default nil :font "Consolas-11.0")
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
