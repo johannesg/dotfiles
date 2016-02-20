e14859ba3f8c8d4a56238f59e6c32951a78d6d02
Johannes Gustafsson <johannesgu@gmail.com>
2016-02-20 20:09:24 +0100

    i3

--- i3/i3status.sh
+++ i3/i3status.sh
@@ -4,6 +4,7 @@ i3status --config ~/.i3/i3status.conf | while :
 do
   read line
   LG=$(setxkbmap -query | awk '/layout/{print $2}')
+  LG=$(~/.dotfiles/bin/xkblayout-state print %s)
   if [ $LG == "se" ]
   then
     dat="[{ \"full_text\": \"LANG: $LG\", \"color\":\"#009E00\" },"
