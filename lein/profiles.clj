56590245d8f04c2d3a4ae8c3c743b4fd09c0f820
Johannes Gustafsson <johannesgu@gmail.com>
2016-02-29 20:56:48 +0100

    lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,9 @@
-{:repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
+{:user {:dependencies [[slamhound "1.3.1"]]
+        :plugins      [[lein-pprint "1.1.1"]
+                       [lein-ancient "0.6.8"]
+                       [lein-cljs-externs "0.1.0"]]}
+
+ :repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
                        [com.cemerick/piggieback "0.2.1"]
                        [weasel "0.7.0"]]
         :plugins      [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]}}
