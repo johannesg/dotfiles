4b82f702051a31a2c71d5df1e351799e2461f4d8
Johannes Gustafsson <johannes@inexchange.se>
2016-02-29 14:10:18 +0100

    lein

---
+++ lein/profiles.clj
@@ -0,0 +1,4 @@
+{:repl {:dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [com.cemerick/piggieback "0.2.1"]
+                       [weasel "0.7.0"]]
+        :plugins      [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]}}
