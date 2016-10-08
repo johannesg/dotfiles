e9f73395d6520c810556ca3918a92519ba9eaad0
Johannes Gustafsson <johannesgu@gmail.com>
2016-10-08 19:56:33 +0200

    fsharp

--- emacs.d/config/init-fsharp.el
+++ emacs.d/config/init-fsharp.el
@@ -2,8 +2,9 @@
 (use-package fsharp-mode
   :mode "\\.fs[x]\\'"
   :config
-  ;; (add-hook 'fsharp-mode-hook #'smartparens-mode)
+  (add-hook 'fsharp-mode-hook #'smartparens-mode)
   ;; (add-hook 'csharp-mode-hook #'omnisharp-mode)
+  (setq-default fsharp-indent-offset 2)
   )
 
 (provide 'init-fsharp)
