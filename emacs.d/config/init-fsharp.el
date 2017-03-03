2539f6afbbf2aa31b7ed2dfe949816ef6412e3fa
Johannes Gustafsson <johannesgu@gmail.com>
2017-03-03 17:31:56 +0100

    fsharp

--- emacs.d/config/init-fsharp.el
+++ emacs.d/config/init-fsharp.el
@@ -5,6 +5,7 @@
   (add-hook 'fsharp-mode-hook #'smartparens-mode)
   ;; (add-hook 'csharp-mode-hook #'omnisharp-mode)
   ;;(setq-default fsharp-indent-offset 2)
+  (add-to-list 'company-transformers 'company-sort-prefer-same-case-prefix)
   )
 
 (provide 'init-fsharp)
