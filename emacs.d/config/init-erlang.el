0db22114a930c98526ef1c1187dd3888130ac18f
Johannes Gustafsson <johannesgu@gmail.com>
2018-03-23 18:34:59 +0100

    erlang

--- emacs.d/config/init-erlang.el
+++ emacs.d/config/init-erlang.el
@@ -1,4 +1,6 @@
 (use-package erlang
+  :mode (("\\.erl\\'" . erlang-mode )
+         ("rebar.config" . erlang-mode ))
   :config
   ;; (use-package distel)
   ;; (use-package company-distel)
@@ -34,16 +36,16 @@
   ;; prevent annoying hang-on-compile
   (defvar inferior-erlang-prompt-timeout t)
   ;; default node name to emacs@localhost
-  (setq inferior-erlang-machine-options '("-sname" "emacs"))
-  ;; tell distel to default to that node
-  (setq erl-nodename-cache
-        (make-symbol
-         (concat
-          "emacs@"
-          ;; Mac OS X uses "name.local" instead of "name", this should work
-          ;; pretty much anywhere without having to muck with NetInfo
-          ;; ... but I only tested it on Mac OS X.
-          (car (split-string (shell-command-to-string "hostname"))))))
+  ; (setq inferior-erlang-machine-options '("-sname" "emacs"))
+  ; ;; tell distel to default to that node
+  ; (setq erl-nodename-cache
+  ;       (make-symbol
+  ;        (concat
+  ;         "emacs@"
+  ;         ;; Mac OS X uses "name.local" instead of "name", this should work
+  ;         ;; pretty much anywhere without having to muck with NetInfo
+  ;         ;; ... but I only tested it on Mac OS X.
+  ;         (car (split-string (shell-command-to-string "hostname"))))))
   
   )
 
