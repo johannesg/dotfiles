1f4ea326eb2440d9305202a31200dbbecf2fea74
Johannes Gustafsson <johannes@jogus.io>
2023-09-25 09:18:32 +0200

    nvim tree

--- nvim/lua/jogus/plugins/misc.lua
+++ nvim/lua/jogus/plugins/misc.lua
@@ -1,35 +1,48 @@
 return {
-	{ 'nvim-telescope/telescope.nvim',
-		tag = '0.1.2',
-		dependencies = { 'nvim-lua/plenary.nvim' } },
-	{ 'rose-pine/neovim', name = 'rose-pine' },
-	{ 'nvim-treesitter/nvim-treesitter',
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        'rose-pine/neovim',
+        name = 'rose-pine'
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
         build = ":TSUpdate",
-        dependencies = { "windwp/nvim-ts-autotag" } },
-	{ 'nvim-treesitter/nvim-treesitter-context'},
-	{ 'theprimeagen/harpoon' },
-	{ 'mbbill/undotree' },
-	{ 'tpope/vim-fugitive' },
-	{ 'tpope/vim-surround' },
-    { 'nvim-tree/nvim-tree.lua', dependencies = {
-        lazy = false,
-        'nvim-tree/nvim-web-devicons'
-        } },
+        dependencies = { "windwp/nvim-ts-autotag" }
+    },
+    { 'nvim-treesitter/nvim-treesitter-context' },
+    { 'theprimeagen/harpoon' },
+    { 'mbbill/undotree' },
+    { 'tpope/vim-fugitive' },
+    { 'tpope/vim-surround' },
+    {
+        'nvim-tree/nvim-tree.lua',
+        dependencies = {
+            lazy = false,
+            'nvim-tree/nvim-web-devicons',
+            config = function()
+                require("nvim-tree").setup {}
+            end,
+        }
+    },
     { 'numToStr/Comment.nvim' },
     {
         -- Set lualine as statusline
-        'nvim-lualine/lualine.nvim', 
+        'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         -- See `:help lualine.txt`
         opts = {
-          options = {
-            icons_enabled = true,
-            theme = 'onedark',
-            component_separators = '|',
- --           section_separators = '',
-            section_separators = { left = '', right = '' },
---            component_separators = { left = '', right = '' }
-          },
+            options = {
+                icons_enabled = true,
+                theme = 'onedark',
+                component_separators = '|',
+                --           section_separators = '',
+                section_separators = { left = '', right = '' },
+                --            component_separators = { left = '', right = '' }
+            },
         }
     },
 
