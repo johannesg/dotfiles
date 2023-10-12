eb7bfe2b8a7f52415d9aab4e8ac5f93da0012b87
Johannes Gustafsson <johannes@jogus.io>
2023-10-12 10:42:55 +0200

    .

--- nvim/lua/jogus/plugins/misc.lua
+++ nvim/lua/jogus/plugins/misc.lua
@@ -17,7 +17,16 @@ return {
     { 'theprimeagen/harpoon' },
     { 'mbbill/undotree' },
     { 'tpope/vim-fugitive' },
-    { 'tpope/vim-surround' },
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
     {
         'nvim-tree/nvim-tree.lua',
         dependencies = {
