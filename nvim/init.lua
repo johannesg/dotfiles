bb19ef8e1e96695e5d24e85bbe1441eb180e034a
Johannes Gustafsson <johannesgu@outlook.com>
2025-05-26 17:38:56 +0200

    lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,2 @@
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
--- package manager
-require("jogus.lazy")
-
-require("jogus.keymappings")
-require("jogus.settings")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
